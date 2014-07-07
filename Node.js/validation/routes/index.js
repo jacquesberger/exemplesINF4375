11e75e7ea69ef34f918c7b68b1803058255022e4
Jacques Berger <jacques.berger@code3.ca>
2014-07-07 12:42:38 -0400

    Exemple de validation JSON avec json-schema et Express

---
+++ Node.js/validation/routes/index.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2014 Jacques Berger.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var jsonschema = require('jsonschema');
+
+var schema = {
+  type: "object",
+  required: true,
+  properties: {
+    age: {
+      type: "number",
+      required: false
+    },
+    nom: {
+      type: "string",
+      required: true
+    },
+    prenom: {
+      type: "string",
+      required: true
+    }
+  }
+}
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.test = function(req, res){
+  res.render('test');
+};
+
+exports.json = function(req, res) {
+  var data = req.body;
+  var validate = require('jsonschema').validate;
+  var result = validate(data, schema);
+  if (result.errors.length === 0) {
+    res.send(201);
+  } else {
+    res.send(400);
+  }
+};
